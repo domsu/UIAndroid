4b4a9ce89d7fec892233554d2e62df0004e09db9
Dominik Suszczewicz <dominik.suszczewicz@gmail.com>
2015-10-31 10:52:37 +0100

    Added number to title

--- app/src/main/java/com/uiandroid/examples/data/ExampleDetailData.java
+++ app/src/main/java/com/uiandroid/examples/data/ExampleDetailData.java
@@ -8,7 +8,7 @@ import com.uiandroid.examples.sample.RevealCustomColorActivity;
 public class ExampleDetailData {
 
     public final static ExampleDetail[] data = new ExampleDetail[]{
-            new ExampleDetail("Reveal effect with custom color", "http://uiandroid.com/reveal-effect-custom-color/", Build.VERSION_CODES.LOLLIPOP, RevealCustomColorActivity.class)
+            new ExampleDetail("1. Reveal effect with custom color", "http://uiandroid.com/reveal-effect-custom-color/", Build.VERSION_CODES.LOLLIPOP, RevealCustomColorActivity.class)
     };
 
 }
